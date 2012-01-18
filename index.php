a2d0f1b264f417249ccf88be74d4bcc74934fe7e
Nathan Kinkade <nkinkade@creativecommons.org>
2012-01-18 19:27:40 +0000

    Removed Anaylytics code, now it is handled via the cc-wp theme.

--- index.php
+++ index.php
@@ -181,8 +181,9 @@ function modRights($engine, $comm, $deriv) {
             echo 'https';
         }
     }
-    include 'cc-wp/meta.php';
-    include 'cc-wp/header-doctype.php'; ?>
+    include_once 'cc-wp/meta.php';
+    include_once 'cc-wp/header-doctype.php'; ?>
+
 <html lang="<?php echo $query_locale ?>">
 	<head>
 		<title>CC Search</title>
@@ -365,17 +366,7 @@ function modRights($engine, $comm, $deriv) {
 <?php include 'cc-wp/page-footer.php'; ?>
     </div> <!--! end of #container -->
 <?php include 'cc-wp/footer-codes.php'; ?>
-<script type="text/javascript"> 
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-2010376-2']);
-	_gaq.push(['_trackPageview']);
- 
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-</script>
+
 </body>
 </html>
 
