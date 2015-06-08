9f698abf39ea8fd01fd2775935165f6780708354
Matt Lee <mattl@cnuk.org>
2015-06-08 16:30:38 -0400

    write line ending

--- bouncer.php
+++ bouncer.php
@@ -4,8 +4,8 @@ $q = $_GET['q'];
 $u = $_GET['url'];
 
 $fp = fopen('queries.txt', 'a');
-fwrite($fp, $q);
-fwrite($fp, $u);
+fwrite($fp, $q . PHP_EOL);
+fwrite($fp, $u . PHP_EOL);
 fclose($fp);
 
 $u = urldecode($u);
