ecb945eca7c1ddac6fcbe458eb29a9c0e99c8d95
imajin14 <imajinarts14@gmail.com>
2022-10-10 18:09:57 +0100

    Resolved all requested changes

--- index.php
+++ index.php
@@ -222,7 +222,7 @@ function modRights($engine, $comm, $deriv) {
 
         <div class="first row">
         <div class="sixteen columns alpha wrong">
-        <h3>Try <a href="https://wordpress.org/openverse/?referrer=creativecommons.org">Openverse</a>: Openly Licensed Images, Audio and More (formerly CC Search Portal).</h3>
+        <h3>Try <a href="https://wordpress.org/openverse/?referrer=creativecommons.org">Openverse</a>: Openly Licensed Images, Audio and More.</h3>
         </div>
         </div>
 
@@ -231,7 +231,7 @@ function modRights($engine, $comm, $deriv) {
 				<form id="search_form" method="get" onsubmit="return doSearch()">
             <div class="seven columns alpha">
 			<div id="header_logo" title="To search, enter some search terms, then click a provider." onclick="if ( $('#query').val() ) { doSearch(); }">
-				<img src="cc-search portal.png" alt="CC Search Portal" />
+				<img src="cc-search.png" alt="CC Search Portal" />
 				<div id="header_text"><span style="color: white;">Find content you can share, use and remix</span></div>
 			</div>
             </div>
