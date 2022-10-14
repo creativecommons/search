0c487682c98d8681d591515141296af0bbf1e626
OsujiPius <piusosuji9@gmail.com>
2022-10-15 00:24:16 +0200

    New footer

--- cc-wp/page-footer.php
+++ cc-wp/page-footer.php
@@ -1,67 +1,50 @@
-		<footer id="fixfooter">
-		<div class="container">
-		
-			<div id="globe"></div>
-
-			<div id="colophon">
-				<div class="sixteen columns">
-					<div class="first row">
-						<div class="five columns">
-							<div class="bucket">
-								<a href="http://www.facebook.com/creativecommons" title="Creative Commons on Facebook"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/facebook.png" alt="Creative Commons on Facebook"/></a>
-								<a href="http://twitter.com/creativecommons" title="Creative Commons on Twitter"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/twitter.png" alt="Creative Commons on Twitter"/></a>
-							</div>
-						</div>
-						<div class="four offset-by-six columns">
-						  <div class="bucket">
-						    <p>
-						      <a rel = "license"
-							 href="http://creativecommons.org/licenses/by/4.0/"
-							 title="Creative Commons Attribution 4.0 International License"
-							 ><img src="//i.creativecommons.org/l/by/4.0/88x31.png"
-							       alt="License"/></a>
-						    </p>
-						    <div style="display: inline;" xmlns:cc="http://creativecommons.org/ns#"
-							 about="http://creativecommons.org">
-						      <p>
-							<small>
-							  Except where otherwise
-							  <a class="subfoot" href="http://creativecommons.org/policies#license">noted</a>,
-							  content on
-							  <span 
-							     href="http://creativecommons.org" 
-							     property="cc:attributionName" 
-							     rel="cc:attributionURL">this site</span> 
-							  is licensed under a 
-							  <a rel="license" 
-							     href="http://creativecommons.org/licenses/by/4.0/"
-							     class="subfoot">
-							    Creative Commons Attribution 4.0 International License</a>.
-							</small>
-						      </p>
-						    </div>
-						  </div>
-						</div>
-					</div>
-				</div>
-				<div class="sixteen columns">
-					<div class="row">
-						<div class="six columns">
-							<ul class="nav">
-							<li><a href="http://creativecommons.org/policies">Policies</a></li>
-							<li><a href="http://creativecommons.org/privacy">Privacy</a></li>
-							<?php
-							if ( preg_match('/(staging\.)?creativecommons\.net/', $_SERVER['HTTP_HOST']) ) {
-								echo '<li><a href="https://creativecommons.net/h/policies/tou">Terms of Use</a></li>';
-							} else {
-								echo '<li><a href="http://creativecommons.org/terms">Terms of Use</a></li>';
-							}
-							?>
-							<li><a href="http://creativecommons.org/contact">Contact CC</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
+	<div class="new_footer">
+		<div class="new_footer_logo">
+			<img src="/img/cc.logo.white.svg" alt="logo">
+			<p>Creative Commons<br/>
+               PO Box 1866, Mountain View CA 94042
+			</p>
+			<a href="mailto:info@creativecommons.org">info@creativecommons.org</a>
+			<a href="tel://+1-415-429-6753">+1-415-429-6753</a>
+			<div class="socials">
+				<a href="https://facebook.com/creativecommons" target=_blank>
+					<img src="/img/facebook-logo.png" alt="facebook-icon">
+				</a>
+				<a href="https://instagram.com/creativecommons" target=_blank>
+					<img src="/img/instagram.png" alt="instagram-icon">
+				</a>
+				<a href="https://linkedin.com/company/creative-commons" target=_blank>
+					<img src="/img/linkedin.png" alt="linkedin-icon">
+				</a>
+				<a href="https://twitter.com/creativecommons" target=_blank>
+					<img src="/img/twitter-sign.png" alt="twitter-icon">
+				</a>
 			</div>
 		</div>
-	</footer>
+		<div class="new_footer_links">
+		 <div className="column">
+            <h3>Quick Links</h3>
+            <ul>
+              <li>
+                  <a href="/blog/" target=_blank>Blog</a> 
+              </li>
+              <li>
+                <a href="/community/community-team" target=_blank>Community Team</a>
+              </li>
+              <li>
+                <a href="/contributing-code/projects" target=_blank>Project List</a>
+              </li>
+              <li>
+                <a href="archives" target=_blank>Archives</a>
+              </li>
+            </ul>
+			<p>Except where otherwise <a href="https://creativecommons.org/policies#license" target=_blank>noted</a>, content on
+				 this site is licensed under a <a href="https://creativecommons.org/licenses/by/4.0/" target=_blank>Creative Commons Attribution 4.0 International license</a>. Icons by <a href="https://fontawesome.com">Font Awesome</a>.</p>
+          </div>
+		</div>
+		<div class="new_footer_donate">
+			<h5>Our work relies on you!</h5>
+			<p>Help us keep the internet free and open.</p>
+			<a href="https://creativecommons.org/donate?c_src=website&amp;c_src2=GlobalFooter" target=_blank class="donate_button">Donate Now!</a>
+		</div>
+	</div>
