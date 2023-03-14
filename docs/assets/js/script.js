12c1b64c95a4fc96f7402e2b72d0e66d68a69b0c
possumbilities <109087089+possumbilities@users.noreply.github.com>
2023-03-14 20:13:38 +0000

    add missing closing bracket in js

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -52,143 +52,144 @@ document.addEventListener("DOMContentLoaded", function(e){
       // engine licenses logic alphabetically
 
       if (form.commercial || form.modify) {
-      switch (form.searchEngine) {
+        switch (form.searchEngine) {
 
-        case "ccmixter":
-          rights = '';
-          if (form.commercial && form.modify) {
-            rights += '';
-          } else if (form.commercial) {
-            rights += '';
-          } else if (form.modify) {
-            rights += '';
-          }
-        break;
+          case "ccmixter":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
 
-        case "europeana":
-          rights = '';
-          if (form.commercial && form.modify) {
-            rights += '';
-          } else if (form.commercial) {
-            rights += '';
-          } else if (form.modify) {
-            rights += '';
-          }
-        break;
+          case "europeana":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
 
-        case "flickr":
-          rights = '&license=';
-          if (form.commercial && form.modify) {
-            rights += '4%2C5%2C9%2C10';
-          } else if (form.commercial) {
-            rights += '4%2C5%2C6%2C9%2C10';
-          } else if (form.modify) {
-            rights += '1%2C2%2C9%2C10';
-          }
-        break;
+          case "flickr":
+            rights = '&license=';
+            if (form.commercial && form.modify) {
+              rights += '4%2C5%2C9%2C10';
+            } else if (form.commercial) {
+              rights += '4%2C5%2C6%2C9%2C10';
+            } else if (form.modify) {
+              rights += '1%2C2%2C9%2C10';
+            }
+          break;
 
-        case "google":
-          rights = '&as_rights=(cc_publicdomain|cc_attribute|cc_sharealike).-';
-          if (form.commercial && form.modify) {
-            rights += '(cc_noncommercial|cc_nonderived)';
-          } else if (form.commercial) {
-            rights += '(cc_noncommercial)';
-          } else if (form.modify) {
-            rights += '(cc_nonderived)';
-          }
-        break;
+          case "google":
+            rights = '&as_rights=(cc_publicdomain|cc_attribute|cc_sharealike).-';
+            if (form.commercial && form.modify) {
+              rights += '(cc_noncommercial|cc_nonderived)';
+            } else if (form.commercial) {
+              rights += '(cc_noncommercial)';
+            } else if (form.modify) {
+              rights += '(cc_nonderived)';
+            }
+          break;
 
-        case "googleimages":
-          rights = '';
-          if (form.commercial && form.modify) {
-            rights += '';
-          } else if (form.commercial) {
-            rights += '';
-          } else if (form.modify) {
-            rights += '';
-          }
-        break;
+          case "googleimages":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
 
-        case "jamendo":
-          rights = '';
-          if (form.commercial && form.modify) {
-            rights += '';
-          } else if (form.commercial) {
-            rights += '';
-          } else if (form.modify) {
-            rights += '';
-          }
-        break;
+          case "jamendo":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
 
-        case "openclipart":
-          rights = '';
-          if (form.commercial && form.modify) {
-            rights += '';
-          } else if (form.commercial) {
-            rights += '';
-          } else if (form.modify) {
-            rights += '';
-          }
-        break;
+          case "openclipart":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
 
-        case "openverse":
-          rights = '&license_type=';
-          if (form.commercial && form.modify) {
-            rights +='commercial,modification';
-          } else if (form.commercial) {
-            rights +='commercial';
-          } else if (form.modify) {
-            rights += 'modification';
-          }
-        break;
+          case "openverse":
+            rights = '&license_type=';
+            if (form.commercial && form.modify) {
+              rights +='commercial,modification';
+            } else if (form.commercial) {
+              rights +='commercial';
+            } else if (form.modify) {
+              rights += 'modification';
+            }
+          break;
 
-        case "soundcloud":
-          rights = '';
-          if (form.commercial && form.modify) {
-            rights += '';
-          } else if (form.commercial) {
-            rights += '';
-          } else if (form.modify) {
-            rights += '';
-          }
-        break;
+          case "soundcloud":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
 
-        case "wikipedia-commons":
-          rights = '';
-          if (form.commercial && form.modify) {
-            rights += '';
-          } else if (form.commercial) {
-            rights += '';
-          } else if (form.modify) {
-            rights += '';
-          }
-        break;
+          case "wikipedia-commons":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
 
-        case "youtube":
-          rights = '';
-          if (form.commercial && form.modify) {
-            rights += '';
-          } else if (form.commercial) {
-            rights += '';
-          } else if (form.modify) {
-            rights += '';
-          }
-        break;
+          case "youtube":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
 
-        // if you want to add a new engine logic,
-        // you can work from this:
+          // if you want to add a new engine logic,
+          // you can work from this:
 
-        // case "default":
-        //   rights = '';
-        //   if (form.commercial && form.modify) {
-        //     rights += '';
-        //   } else if (form.commercial) {
-        //     rights += '';
-        //   } else if (form.modify) {
-        //     rights += '';
-        //   }
-        // break;
+          // case "default":
+          //   rights = '';
+          //   if (form.commercial && form.modify) {
+          //     rights += '';
+          //   } else if (form.commercial) {
+          //     rights += '';
+          //   } else if (form.modify) {
+          //     rights += '';
+          //   }
+          // break;
+        }
+        return rights;
       }
-      return rights;
     }
 });
