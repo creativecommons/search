613e4b8261b8f940718d48cbd97262b5fffaa35d
possumbilities <109087089+possumbilities@users.noreply.github.com>
2023-03-14 19:34:38 +0000

    wrap basic logic, setup engine rights behavior scaffold

--- docs/temp.js
+++ docs/temp.js
@@ -41,17 +41,154 @@ document.addEventListener("DOMContentLoaded", function(e){
           }
 
         let licenseParams = getLicenseParams(form);
-
         return  form.searchEngineURL + form.query + licenseParams;
       }
 
       // construct license params by engine convention
       function getLicenseParams(form) {
 
-        // prolly gonna need a switch here to handle this per engine, with a default fallback
+        let rights;
 
+        // engine licenses logic alphabetically
 
-        return '';
+        if (form.commercial || form.modify) {
+        switch (form.searchEngine) {
+
+          case "ccmixter":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
+
+          case "europeana":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
+
+          case "flickr":
+            rights = '&license=';
+            if (form.commercial && form.modify) {
+              rights += '4%2C5%2C9%2C10';
+            } else if (form.commercial) {
+              rights += '4%2C5%2C6%2C9%2C10';
+            } else if (form.modify) {
+              rights += '1%2C2%2C9%2C10';
+            }
+          break;
+
+          case "google":
+            rights = '&as_rights=(cc_publicdomain|cc_attribute|cc_sharealike).-';
+            if (form.commercial && form.modify) {
+              rights += '(cc_noncommercial|cc_nonderived)';
+            } else if (form.commercial) {
+              rights += '(cc_noncommercial)';
+            } else if (form.modify) {
+              rights += '(cc_nonderived)';
+            }
+          break;
+
+          case "googleimages":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
+
+          case "jamendo":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
+
+          case "openclipart":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
+
+          case "openverse":
+            rights = '&license_type=';
+            if (form.commercial && form.modify) {
+              rights +='commercial,modification';
+            } else if (form.commercial) {
+              rights +='commercial';
+            } else if (form.modify) {
+              rights += 'modification';
+            }
+          break;
+
+          case "soundcloud":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
+
+          case "wikipedia-commons":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
+
+          case "youtube":
+            rights = '';
+            if (form.commercial && form.modify) {
+              rights += '';
+            } else if (form.commercial) {
+              rights += '';
+            } else if (form.modify) {
+              rights += '';
+            }
+          break;
+
+          // if you want to add a new engine logic,
+          // you can work from this:
+
+          // case "default":
+          //   rights = '';
+          //   if (form.commercial && form.modify) {
+          //     rights += '';
+          //   } else if (form.commercial) {
+          //     rights += '';
+          //   } else if (form.modify) {
+          //     rights += '';
+          //   }
+          // break;
+        }
+        return rights;
       }
-
 });
