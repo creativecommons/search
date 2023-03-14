d4bfa77c11aa200f80b96dd332189ab2fd226b00
possumbilities <109087089+possumbilities@users.noreply.github.com>
2023-03-14 19:49:53 +0000

    pull js from temp, adjust index to match

--- docs/script.js
+++ docs/script.js
@@ -1,72 +1,194 @@
-const searchBox = document.querySelector("#search-value");
-const engine = document.querySelector("[data-object]");
-const searchButton = document.querySelector("#searchBtn");
-let data = document.querySelectorAll("[data-engine]");
-let searchInput;
-let link;
-let card;
+document.addEventListener("DOMContentLoaded", function(e){
 
-engine.addEventListener("click", onActiveState);
-function onActiveState(e) {
-  //engine.style.backgroundColor = "red"; //DEBUG
-  engine.classList.toggle;
-  console.log(engine.className);
-  //searchButton.stlye.backgroundColor = "red"; //DEBUG
-}
+  const searchForm = document.getElementById("searchForm");
 
-// add event listener to the search box to know what the user is typing
-searchBox.addEventListener("input", searchEngine);
-function searchEngine(e) {
-  console.log(e.target.value); //DEBUG
-  // create the state of the link
-  searchInput = e.target.value;
-}
+  searchForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+    
+      // capture and process the submission
+      console.log('captured cleanly!');
 
-// add event listener to the search button to know when the user clicks on it
-searchButton.addEventListener("click", Navigate);
-// create the function to open the link in a new tab
-function Navigate() {
-  if (searchInput === undefined) {
-    alert("Please enter a search value");
-    return;
-  }
-  if (link === undefined) {
-    alert("Please select a search engine");
-    return;
-  }
-  window.open(link, "_blank");
-}
+      let form = {};
+      form.query = document.getElementById("query").value;
+      form.commercial = document.getElementById("commercial").checked;
+      form.modify = document.getElementById("modify").checked;
+      selectedEngine = document.querySelector('input[name="search-engine"]:checked');
+      form.searchEngine = selectedEngine.value;
+      form.searchEngineURL = selectedEngine.dataset.url;
 
-// add event listener to the cards to know when the user clicks on anyone of
-// them
-for (let i = 0; i < data.length; i++) {
-  data[i].addEventListener("click", cardClick);
-}
+      // build URL & navigate to link
+      let link = buildURL(form);
+      navigateTo(link);
+    });
 
-// create the function to get the data from the cards
-function cardClick(e) {
-  let searchUrl, searchEngine;
-  console.log(e.target); //DEBUG
-  if (e.target.tagName === "p") {
-    searchUrl = e.target.parentNode.getAttribute("data-url");
-    searchEngine = e.target.parentNode.getAttribute("data-engine");
-  } else {
-    searchUrl = e.target.getAttribute("data-url");
-    searchEngine = e.target.getAttribute("data-engine");
-  }
-  console.log({ searchEngine, searchUrl, searchInput }); //DEBUG
-  link = getURL(searchEngine, searchUrl, searchInput);
-  console.log(link); //DEBUG
-}
+    // navigate behavior
+    function navigateTo(link) {
 
-// create the function to get the link and build the search query from the
-// card that the user clicked on
-function getURL(value, searchUrl, input) {
-  switch (value) {
-    case null:
-    case "":
-      return "Invalid value";
-    default:
-      return `${searchUrl}${input}`;
-  }
-}
+      if (link) {
+          window.open(link, "_blank");
+      }
+    }
+
+    // construct the URL
+    function buildURL(form) {
+      if (form.query === '') {
+          alert("Please enter a search value");
+          return;
+        }
+        if (form.searchEngineURL === undefined) {
+          alert("Please select a search engine");
+          return;
+        }
+
+      let licenseParams = getLicenseParams(form);
+      return  form.searchEngineURL + form.query + licenseParams;
+    }
+
+    // construct license params by engine convention
+    function getLicenseParams(form) {
+
+      let rights;
+
+      // engine licenses logic alphabetically
+
+      if (form.commercial || form.modify) {
+      switch (form.searchEngine) {
+
+        case "ccmixter":
+          rights = '';
+          if (form.commercial && form.modify) {
+            rights += '';
+          } else if (form.commercial) {
+            rights += '';
+          } else if (form.modify) {
+            rights += '';
+          }
+        break;
+
+        case "europeana":
+          rights = '';
+          if (form.commercial && form.modify) {
+            rights += '';
+          } else if (form.commercial) {
+            rights += '';
+          } else if (form.modify) {
+            rights += '';
+          }
+        break;
+
+        case "flickr":
+          rights = '&license=';
+          if (form.commercial && form.modify) {
+            rights += '4%2C5%2C9%2C10';
+          } else if (form.commercial) {
+            rights += '4%2C5%2C6%2C9%2C10';
+          } else if (form.modify) {
+            rights += '1%2C2%2C9%2C10';
+          }
+        break;
+
+        case "google":
+          rights = '&as_rights=(cc_publicdomain|cc_attribute|cc_sharealike).-';
+          if (form.commercial && form.modify) {
+            rights += '(cc_noncommercial|cc_nonderived)';
+          } else if (form.commercial) {
+            rights += '(cc_noncommercial)';
+          } else if (form.modify) {
+            rights += '(cc_nonderived)';
+          }
+        break;
+
+        case "googleimages":
+          rights = '';
+          if (form.commercial && form.modify) {
+            rights += '';
+          } else if (form.commercial) {
+            rights += '';
+          } else if (form.modify) {
+            rights += '';
+          }
+        break;
+
+        case "jamendo":
+          rights = '';
+          if (form.commercial && form.modify) {
+            rights += '';
+          } else if (form.commercial) {
+            rights += '';
+          } else if (form.modify) {
+            rights += '';
+          }
+        break;
+
+        case "openclipart":
+          rights = '';
+          if (form.commercial && form.modify) {
+            rights += '';
+          } else if (form.commercial) {
+            rights += '';
+          } else if (form.modify) {
+            rights += '';
+          }
+        break;
+
+        case "openverse":
+          rights = '&license_type=';
+          if (form.commercial && form.modify) {
+            rights +='commercial,modification';
+          } else if (form.commercial) {
+            rights +='commercial';
+          } else if (form.modify) {
+            rights += 'modification';
+          }
+        break;
+
+        case "soundcloud":
+          rights = '';
+          if (form.commercial && form.modify) {
+            rights += '';
+          } else if (form.commercial) {
+            rights += '';
+          } else if (form.modify) {
+            rights += '';
+          }
+        break;
+
+        case "wikipedia-commons":
+          rights = '';
+          if (form.commercial && form.modify) {
+            rights += '';
+          } else if (form.commercial) {
+            rights += '';
+          } else if (form.modify) {
+            rights += '';
+          }
+        break;
+
+        case "youtube":
+          rights = '';
+          if (form.commercial && form.modify) {
+            rights += '';
+          } else if (form.commercial) {
+            rights += '';
+          } else if (form.modify) {
+            rights += '';
+          }
+        break;
+
+        // if you want to add a new engine logic,
+        // you can work from this:
+
+        // case "default":
+        //   rights = '';
+        //   if (form.commercial && form.modify) {
+        //     rights += '';
+        //   } else if (form.commercial) {
+        //     rights += '';
+        //   } else if (form.modify) {
+        //     rights += '';
+        //   }
+        // break;
+      }
+      return rights;
+    }
+});
